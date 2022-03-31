611f9e6c7480a3b2e0436e95fb748e8810661d5f
Colt Dorsey <colt.dorsey@onxmaps.com>
2022-03-31 14:42:36 -0500

    Bug fix for project keys given as lowercase. Added tests.

--- index.js
+++ index.js
@@ -7,23 +7,17 @@ async function run() {
     const regexp = /^[.A-Za-z0-9_-]*$/
 
     if (!changelog) {
-      core.setFailed(
-        'changelog property is required. Generate the changelog with github action coltdorsey/jirafy-changelog and reference the step output.'
-      )
+      core.setFailed('changelog property is required. Generate the changelog with github action coltdorsey/jirafy-changelog and reference the step output.')
     }
 
     if (!jiraVersion) {
-      core.setFailed(
-        'jiraVersion property is required. Try setting the value to ${{ github.event.release.tag_name }}'
-      )
+      core.setFailed('jiraVersion property is required. Try setting the value to ${{ github.event.release.tag_name }}')
     }
 
     if (!!jiraVersion && regexp.test(jiraVersion)) {
       await syncChangelogToJira(changelog, jiraVersion)
     } else {
-      core.setFailed(
-        'Branch names must contain only numbers, strings, underscores, periods, and dashes.'
-      )
+      core.setFailed('Branch names must contain only numbers, strings, underscores, periods, and dashes.')
     }
   } catch (error) {
     core.setFailed(error.message)
---
+++ test/jirafy-sync.spec.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const jira = require('../utils/jira')
+const chai = require('chai')
+const key = 'JSYNC'
+const badKey = 'arglebargle'
+
+describe('Jirafy Sync', () => {
+  context('Jira', () => {
+    it('getProjectIdByKey - Ensure jira project keys are always uppercase', async () => {
+      var resp = await jira.getProjectIdByKey(key.toLowerCase())
+      chai.assert.equal(resp, '10002')
+    })
+
+    it('getProjectIdByKey - No project error', async () => {
+      var resp = await jira.getProjectIdByKey(badKey)
+      chai.assert.equal(resp, `Error: No project could be found with key '${badKey.toUpperCase()}'.`)
+    })
+
+    it('getProjectIdByKey - success', async () => {
+      var resp = await jira.getProjectIdByKey('jsync')
+      chai.assert.equal(resp, '10002')
+    })
+  })
+})
