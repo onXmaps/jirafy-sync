7cdade85cbb7ad88ee4d79c3cbd977d9b4b2f72f
Colt Dorsey <colt.dorsey@onxmaps.com>
2022-04-29 15:26:47 -0500

    projectId test

--- test/jirafy-sync.spec.js
+++ test/jirafy-sync.spec.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const jira = require('../utils/jira')
 const { parseChangelogForJiraTickets } = require('../utils/jira-helper')
 const chai = require('chai')
+const core = require('@actions/core')
 const key = 'SDET'
 const badKey = 'arglebargle'
 
 describe('Jirafy Sync', () => {
+  var projectId
   var changelog =
     '* [`4f94449`](http://github.com/onXmaps/jirafy-changelog/commit/4f94449419698e134785e58ceac74702ab47781b)  [`JIRAFY-5`](https://coltdorsey.atlassian.net/browse/JIRAFY-5) Readme example release update - Merge pull request #5 from coltdorsey/colt/readme-example-release \
   * [`58e69be`](http://github.com/onXmaps/jirafy-changelog/commit/58e69be6433c786c2618536dc1d1dbbdde30a461)  [`JIRAFY-6`](https://coltdorsey.atlassian.net/browse/JIRAFY-6) Image reference fix - Merge pull request #6 from coltdorsey/colt/image-reference-fix \
@@ -16,10 +18,16 @@ describe('Jirafy Sync', () => {
   * [`2616445`](http://github.com/onXmaps/jirafy-changelog/commit/26164452d005d08b01bc266abc49de2eae70260c) Update dependency @vercel/ncc to v0.33.0 - Merge pull request #10 from coltdorsey/renovate/vercel-ncc-0.x \
   * [`ce73404`](http://github.com/onXmaps/jirafy-changelog/commit/ce73404ce5cb38c9c6a86b6188a790e76a7575fb)  [`SDET-486`](https://coltdorsey.atlassian.net/browse/SDET-486) Updating references - Merge pull request #22 from onXmaps/colt/update-references'
 
+  before(() => {
+    core.setSecret('projectId')
+    projectId = process.env.PROJECT_ID
+  })
+
   context('Jira Projects', () => {
     it('Ensure jira project keys are always uppercase', async () => {
       var resp = await jira.getProjectIdByKey(key.toLowerCase())
-      chai.assert.equal(resp, process.env.PROJECT_ID)
+      chai.assert.equal(resp, process.env.PROJECT_ID || projectId)
+      core.set
     })
 
     it('Ensure project retrieval error message', async () => {
@@ -29,7 +37,7 @@ describe('Jirafy Sync', () => {
 
     it('Ensure project retrieval success', async () => {
       var resp = await jira.getProjectIdByKey(key)
-      chai.assert.equal(resp, process.env.PROJECT_ID)
+      chai.assert.equal(resp, process.env.PROJECT_ID || projectId)
     })
   })
 
